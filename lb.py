181e248d2baf3fcae9ed50332a0ac352241f9063
linuxNeutrou <itsneutrouthetrou@gmail.com>
2025-07-11 14:54:25 +0200

    fixed invalid I/O operation

--- lb.py
+++ lb.py
@@ -5,7 +5,7 @@ def main():
 		data = json.load(file)
 	data.sort(key=lambda x: x[2], reverse=True)
 	for i in range(0, len(data)):
-		print(f"top {i+1}: {data[i][1]} {data[i][2]}", file=file)
+		print(f"top {i+1}: {data[i][1]} {data[i][2]}")
 
 if __name__ == "__main__":
 	main()
